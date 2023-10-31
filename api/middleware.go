b28b1c63f14d1b351e37541049c6394236704e2e
nabilpatel4012 <nabilpatel4012@gmail.com>
2023-10-31 13:06:49 +0530

    add cors

--- api/middleware.go
+++ api/middleware.go
@@ -18,7 +18,7 @@ const (
 
 func corsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173, http://localhost:4173, http://localhost:81")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
